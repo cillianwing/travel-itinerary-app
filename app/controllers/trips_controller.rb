8d6e29ea84ebc5b845788234e9bb5285930851d9
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-07-18 23:09:11 -0500

    Add basic views to trips view folder.

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,36 @@
 class TripsController < ApplicationController
+  before_action :set_trip!, only: [:show, :edit, :update, :destroy]
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:name, :description, :start_date, :end_date, :total_cost, :user_id)
+  end
+
+  def set_trip!
+    @trip = Trip.find(params[:id])
+  end
+
 end
