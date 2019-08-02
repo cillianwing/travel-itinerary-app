3e04f3e304dc3d87d8c2a703265015072dacf14f
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-08-01 21:48:18 -0500

    Stub out basic methods within TripsController and added require_login before_action

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,3 +1,25 @@
 class TripsController < ApplicationController
+  before_action :require_login
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 
 end
