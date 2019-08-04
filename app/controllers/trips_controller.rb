5c61ac28d076e3ec40e096902059bc3bba4970c3
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-08-04 15:37:59 -0500

    Update trips#index such that the logged in user can only view their trips, not trips associated with other users. If they try to go to url of another user's trips, they are directed to their show page and an alert is given. Add hidden_field for :user_id to trips#index view.

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,18 +2,28 @@ class TripsController < ApplicationController
   before_action :require_login
 
   def index
-    @trips = current_user.trips
+    if current_user.id == params[:user_id].to_i
+      @trips = current_user.trips
+    else
+      redirect_to user_path(current_user), alert: "You cannot view another user's trips."
+    end
   end
 
   def new
-    @trip = Trip.new
+    @trip = Trip.new(user_id: params[:user_id])
   end
 
   def create
-    
+    @trip = current_user.trips.new(trip_params)
+    if @trip.save
+      redirect_to @trip
+    else
+      render :new
+    end
   end
 
   def show
+    @trip = Trip.find(params[:id])
   end
 
   def edit
@@ -23,6 +33,12 @@ class TripsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:name, :description, :start_date, :end_date, :user_id)
   end
 
 end
