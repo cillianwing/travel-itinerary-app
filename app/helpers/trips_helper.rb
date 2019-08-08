453db893ff9e8df14b652d57b0d3dbb5ee56b8c2
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-08-07 22:06:23 -0500

    Created validation helper method to confirm a new trip's start_date is before the end_date.

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -1,2 +1,13 @@
 module TripsHelper
+
+  def start_date_before_end_date
+    if start_date > end_date
+      errors.add(:start_date, "cannot be after end date")
+    end
+  end
+
+  def date_confirm
+    
+  end
+
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,5 @@
 class Trip < ApplicationRecord
+  include TripsHelper
   belongs_to :user
   # will need to add relationship to flights next
 
@@ -6,5 +7,9 @@ class Trip < ApplicationRecord
   validates :description, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
+  validate :start_date_before_end_date
+
+  # need validation that checks if a new trip has dates that fall within the dates of an existing trip
+  # if trip exists, direct user to the existing trip page
 
 end
