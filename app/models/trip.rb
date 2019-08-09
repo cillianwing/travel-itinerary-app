662e1b1cc8f74c4265e9b6202b26261884bafd23
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-08-08 20:23:06 -0500

    Add custom validation that checks whether new trip's start/end dates fall within an existing trips dates. Throws an error if it does.

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -7,7 +7,7 @@ class Trip < ApplicationRecord
   validates :description, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
-  validate :start_date_before_end_date
+  validate :start_date_before_end_date, :date_confirm
 
   # need validation that checks if a new trip has dates that fall within the dates of an existing trip
   # if trip exists, direct user to the existing trip page
