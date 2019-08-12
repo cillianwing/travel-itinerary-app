640a42e45739af660707762ec07901f28b4f5606
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-08-11 21:30:00 -0500

    Add flights routes to routes.rb and route methods to flights controller.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users do
     resources :trips
-    resources :flights
   end
+
+  resources :flights
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
