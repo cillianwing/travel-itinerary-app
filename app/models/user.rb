2a680ae9118801fc0d89ae30312a5fb76d327565
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-08-20 18:52:41 -0500

    Add in the code needed for GitHub OmniAuth capabilities.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,11 @@ class User < ApplicationRecord
   validates :name, :password, presence: true
   validates :email, presence: true, uniqueness: { message: "%{attribute} already taken." }
 
+  def self.find_or_create_by_omniauth(auth_hash)
+    self.where(email: auth_hash["info"]["email"]).first_or_create do |user|
+      user.password = SecureRandom.hex
+    end
+    #returns instance of user that was found or just created
+  end
+
 end
--- config/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, '7c1cf9fa2a1770a9dfb2', '0b1f37ed53e76fcf1144415c5bb8a0786b69df42'
-end
