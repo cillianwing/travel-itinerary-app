b8851c4c20abf9ba9342182c14c1c8f8563d1622
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-08-21 00:20:43 -0500

    Update trips#new and trips#edit to include flash and field_with_errors messages if a user does not enter valid data.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
 
   def home
+    redirect_to users_path if logged_in?
   end
-  
+
 end
--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -11,14 +11,16 @@ module TripsHelper
     @user.trips.each do |trip|
       if trip.id != self.id
         if start_date > trip.start_date && start_date < trip.end_date
-          errors.add(:start_date, "cannot be during an existing trip")
+          errors.add(:start_date, "#{:start_date.to_s.split("_").join(" ").capitalize} cannot be during an existing trip.")
         elsif end_date > trip.start_date && end_date < trip.end_date
-          errors.add(:end_date, "cannot be during an existing trip")
+          errors.add(:end_date, "#{:end_date.to_s.split("_").join(" ").capitalize} cannot be during an existing trip.")
         elsif start_date < trip.start_date && end_date > trip.end_date
-          errors.add(:trip_dates, "Trip dates cannot be overlapping with an existing Trip.")
+          errors.add(:trip_dates, "#{:trip_dates.to_s.split("_").join(" ").capitalize} cannot be overlapping with an existing trip.")
         end
       end
     end
   end
 
 end
+
+#{attr.to_s.split("_").join(" ").capitalize}
