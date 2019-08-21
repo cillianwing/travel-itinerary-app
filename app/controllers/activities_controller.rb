95c9002296aced89604569285ebc6f6b64f10cd3
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-08-20 23:40:27 -0500

    Added styling to the trips#new view page to look similar to user creation/login.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,4 +1,6 @@
 class ActivitiesController < ApplicationController
+  include ActivitiesHelper
+  scope :booked, -> { where(booked: true) }
   before_action :require_login
   before_action :set_activity, only: [:show, :edit, :update, :destroy]
 
