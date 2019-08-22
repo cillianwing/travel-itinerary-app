cd57da2911b4d7a0e9c2bb52ae7b1b44fdf168ae
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-08-21 23:59:13 -0500

    Added necessary presence and date confirmation validations to Accommodation model.

--- app/helpers/accommodations_helper.rb
+++ app/helpers/accommodations_helper.rb
@@ -1,2 +1,19 @@
 module AccommodationsHelper
+
+  def arrival_departure_confirm
+    if departure_date > arrival_date
+      errors.add(:departure_date, "cannot be after arrival date")
+    end
+  end
+
+  def accommodation_trip_date_confirm
+    # confirm flight dates are within the dates of the current trip
+    @trip = Trip.find_by(id: self.trip_ids.last)
+    if departure_date < @trip.start_date || departure_date > @trip.end_date
+      errors.add(:departure_date, "cannot be outside dates of this trip")
+    elsif arrival_date < @trip.start_date || arrival_date > @trip.end_date
+      errors.add(:arrival_date, "cannot be outside dates of this trip")
+    end
+  end
+
 end
--- app/models/accommodation.rb
+++ app/models/accommodation.rb
@@ -2,4 +2,13 @@ class Accommodation < ApplicationRecord
   include AccommodationsHelper
   has_many :reservations
   has_many :trips, through: :reservations
+
+  validates :name, :type, :location, :arrival_date, :departure_date, :check_in, :check_out, :cost, :address, presence: true
+  validate :arrival_departure_confirm, :accommodation_trip_date_confirm
+
+  def book_reservation(trip)
+    # method to be used once user enters a (booked) flight for a specific trip
+    @reservation = Reservation.create(trip_id: trip.id, accommodation_id: self.id)
+  end
+
 end
