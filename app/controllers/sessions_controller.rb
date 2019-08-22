3429f3884ad6bddecb25c3c1d8a6db4566a18537
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-08-22 07:07:30 -0500

    Finished OmniAuth github setup and added flash message upon successful login that asks the user to update the rest of their profile.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
     if auth_hash = request.env["omniauth.auth"]
       user = User.find_or_create_by_omniauth(auth_hash)
       session[:user_id] = user.id
-      redirect_to users_path
+      redirect_to user_path(user), :flash => { message: "Please update profile information or return home to start planning your trips!" }
     else
       @user = User.find_by(email: params[:user][:email])
       if @user
