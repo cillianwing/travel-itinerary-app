827353a2b1013bd54a7b6d2610c4f7828b7364e2
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-08-22 00:03:58 -0500

    Added all CRUD functionality methods to AccommodationsController.

--- app/controllers/accommodations_controller.rb
+++ app/controllers/accommodations_controller.rb
@@ -1,2 +1,55 @@
 class AccommodationsController < ApplicationController
+  before_action :require_login
+  before_action :set_accommodation, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @accommodations = current_trip.accommodations.sort_by { |hash| hash[:arrival] }
+  end
+
+  def show
+  end
+
+  def new
+    @accommodation = current_trip.accommodations.new
+  end
+
+  def create
+    @accommodation = current_trip.accommodations.new(accommodation_params)
+    @accommodation.trip_ids << current_trip.id
+    if @accommodation.save
+      @accommodation.book_reservation(current_trip)
+      current_trip.update_total(@accommodation.cost)
+      redirect_to trip_accommodation_path(current_trip, @accommodation)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @accommodation.update(accommodation_params)
+      redirect_to trip_accommodation_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    current_trip.subtract_total(@accommodation.cost)
+    @accommodation.destroy
+    redirect_to trip_accommodations_path(current_trip)
+  end
+
+  private
+
+  def accommodation_params
+    params.require(:accommodation).permit(:name, :type, :location, :arrival_date, :departure_date, :check_in, :check_out, :cost, :address, :booked)
+  end
+
+  def set_accommodation
+    @accommodation = Accommodation.find(params[:id])
+  end
+
 end
