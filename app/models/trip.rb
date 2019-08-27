7da34937806cb1262ee136c52e6b732922d95cde
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-08-26 20:19:53 -0500

    Update styling for trips#index view page to list each of the user's trips and allow them to select a trip to view details of. Added additional images to asset pipeline.

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -20,4 +20,33 @@ class Trip < ApplicationRecord
     self.update(total_cost: self.total_cost -= expense)
   end
 
+  def select_image
+    month = self.start_date.to_s.split("-")[1].to_i
+    if month == 1
+      image_url = "trip1.jpg"
+    elsif month == 2
+      image_url = "trip2.jpg"
+    elsif month == 3
+      image_url = "trip3.jpg"
+    elsif month == 4
+      image_url = "trip4.jpg"
+    elsif month == 5
+      image_url = "trip5.jpg"
+    elsif month == 6
+      image_url = "trip6.jpg"
+    elsif month == 7
+      image_url = "trip7.jpg"
+    elsif month == 8
+      image_url = "trip8.jpg"
+    elsif month == 9
+      image_url = "trip9.jpg"
+    elsif month == 10
+      image_url = "trip10.jpg"
+    elsif month == 11
+      image_url = "trip11.jpg"
+    elsif month == 12
+      image_url = "trip12.jpg"
+    end
+  end
+
 end
